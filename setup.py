fb480b6c380603c78977d4d94ac5e1ab77d02980
Alan Stockwell <alan@decodeit.co.nz>
2026-01-08 12:07:44 +1300

    alter required django version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     license='BSD',
     packages=find_packages(),
     install_requires=[
-        'django>=3.2,<5.3',
+        'django>=3.2',
         'pyodbc>=3.0',
         'pytz',
     ],
